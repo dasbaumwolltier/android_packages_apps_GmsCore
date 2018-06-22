483f7dfdfd84108cc82ff55116deb68c9b0eac08
dasbaumwolltier <->
2018-06-22 15:47:57 +0200

    test

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd /home/travis/build/dasbaumwolltier/android_packages_apps_GmsCore/
+HOMEPATH="/home/travis/build/dasbaumwolltier/android_packages_apps_GmsCore/"
 
-mkdir -p microg
-cd microg
-mkdir -p outputs
-mkdir -p keystores
+mkdir -p $HOMEPATH/microg
+cd $HOMEPATH/microg
+mkdir -p $HOMEPATH/outputs
+mkdir -p $HOMEPATH/keystores
 
-ANDROID_SDK_PATH="/opt/android-sdk-update-manager"
-KEYSTORES_PATH="keystores"
+ANDROID_SDK_PATH="$ANDROID_HOME"
+KEYSTORES_PATH="$HOMEPATH/keystores"
 
 # Generate keystore
-sudo apt install tree -y && tree || ls -la *
+sudo apt install tree -y && tree || ls -la $HOMEPATH/*
 echo "We need to create a keystore for GmsCore:"
 yes|keytool -genkey -v -keystore $KEYSTORES_PATH/playservices.jks -alias playservices -keyalg RSA -keysize 4096 -validity 10000 -keypass "itecceti123#GmsCore" -storepass "itecceti123#GmsCore"
 
@@ -23,10 +23,10 @@ yes|keytool -genkey -v -keystore $KEYSTORES_PATH/playservices.jks -alias playser
 #echo "sdk.dir=$ANDROID_SDK_PATH" > local.properties
 #echo "sdk-location=$ANDROID_SDK_PATH" >> local.properties
 #./gradlew build
-cp play-services-core-release/build/outputs/apk/*-unsigned.apk ../outputs/play-services-core-release.apk
+cp $HOMEPATH/play-services-core-release/build/outputs/apk/*-unsigned.apk $HOMEPATH/outputs/play-services-core-release.apk
 
 
 # Sign APK
-cd ../outputs
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORES_PATH/playservices.jks play-services-core-release.apk playservices -keypass "itecceti123#GmsCore" -storepass "itecceti123#GmsCore"
-$ANDROID_SDK_PATH/build-tools/27.0.3/zipalign -p -v 4 play-services-core-release.apk com.google.android.gms.apk
+cd $HOMEPATH/outputs
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORES_PATH/playservices.jks $HOMEPATH/outputs/play-services-core-release.apk playservices -keypass "itecceti123#GmsCore" -storepass "itecceti123#GmsCore"
+$ANDROID_SDK_PATH/build-tools/27.0.3/zipalign -p -v 4 $HOMEPATH/outputs/play-services-core-release.apk $HOMEPATH/outputs/com.google.android.gms.apk
